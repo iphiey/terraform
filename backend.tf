9796f5f8863d32f345cb6d42ada1149c05ed2bbb
iphiey <116268633+iphiey@users.noreply.github.com>
2023-09-17 08:35:46 -0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-  terraform {
-    backend "s3" {
-     bucket = "iphiey-jjtech-bucket"
-     key    = "state.tfstate"
-     region = "us-east-1"
-     profile = "default"
- dynamodb_table= "iphiey-jjtech-table"
-    }
-  }
+ # terraform {
+  #  backend "s3" {
+   #  bucket = "iphiey-jjtech-bucket"
+    # key    = "state.tfstate"
+     #region = "us-east-1"
+     #profile = "default"
+ #dynamodb_table= "iphiey-jjtech-table"
+  #  }
+  #}
 
